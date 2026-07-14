78fa8d417241d35dc34d0d24a1d3dd3885b299b9
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-14 16:21:46 -0500

    commit - Perro .

--- src/main/java/com/ejemplo/animales/Animal.java
+++ src/main/java/com/ejemplo/animales/Animal.java
@@ -1,4 +1,22 @@
 package main.java.com.ejemplo.animales;
 
 public class Animal {
+
+    protected String nombre;
+    protected  int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarDatos() {
+
+        System.out.println("Nombre : " + nombre);
+        System.out.println("Edad   : " + edad);
+    }
+
+    public void hacerSonido() {
+        System.out.println("El animal hace un sonido.");
+    }
 }
--- src/main/java/com/ejemplo/animales/Perro.java
+++ src/main/java/com/ejemplo/animales/Perro.java
@@ -1,4 +1,17 @@
 package main.java.com.ejemplo.animales;
 
-public class Perro {
+public class Perro extends Animal {
+
+    public Perro(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(nombre + " dice: Guau Guau");
+    }
+
+    public void correr() {
+        System.out.println(nombre + " está corriendo.");
+    }   
 }
